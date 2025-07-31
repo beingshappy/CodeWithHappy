b2a607dd7e11bddb10f9b81342492040b196de48
Sahasransu Satpathy <satpathy707@gmail.com>
2025-07-31 22:03:13 +0530

    add footer

--- script.js
+++ script.js
@@ -814,4 +814,21 @@ function closePopup() {
   popup.classList.add("hidden");
 }
 
-// project sec
+// support sec
+document.addEventListener("DOMContentLoaded", function () {
+    const copyBtn = document.getElementById("copy-upi");
+    const upiID = document.getElementById("upi-id");
+    const copyMsg = document.getElementById("copy-msg");
+
+    copyBtn.addEventListener("click", function () {
+      const upiText = upiID.textContent;
+      navigator.clipboard.writeText(upiText).then(() => {
+        copyMsg.style.display = "inline";
+        setTimeout(() => {
+          copyMsg.style.display = "none";
+        }, 2000);
+      }).catch(err => {
+        alert("Failed to copy: " + err);
+      });
+    });
+  });
